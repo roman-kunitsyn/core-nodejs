181a004048d2b2ea3a613af6d655e9969ba2bfcb
Roman Kunitsyn <roman.kunitsyn@gmail.com>
2018-08-24 17:24:10 +0300

    work flow

--- server-router/index.js
+++ server-router/index.js
@@ -1,38 +1,76 @@
 module.exports.router = router;
 
 function router(req, res) {
-  let navigation = getNavigation(req);
-  switcher(navigation, req, res);
+  staticRouter(req, res);
+  authRouter(req, res);
+  crudRouter(req, res);
+  errorHandler(req, res);
 }
 
-function getNavigation(req) {
-  let navigation = {
-    url: req.url,
-    method: req.method
-  };
-  return navigation;
-}
-
-function switcher(navigation, req, res) {
-  switch (navigation.url) {
-    case "/":
-      res.end("root");
+function staticRouter(req, res) {
+  switch (`${req.method} ${req.url}`) {
+    case "GET /home":
+      res.end("GET /home");
       break;
-    case "/test":
-      res.end("test");
+    case "GET /registration":
+      res.end("GET /registration");
       break;
-    case "/login":
-      res.end("login");
+    case "GET /login":
+      res.end("GET /login");
       break;
-    case "/logout":
-      res.end("logout");
-      break;
-    case "/registration":
-      res.end("registration");
-      break;
-    default:
-      res.end(`404 not found ${navigation.url}`);
+    case "GET /work":
+      res.end("GET /work");
       break;
   }
   return;
+}
+
+function authRouter(req, res) {
+  switch (`${req.method} ${req.url}`) {
+    case "POST /auth/registration":
+      res.end("POST /registration");
+      break;
+    case "POST /auth/login":
+      res.end("POST /login");
+      break;
+    case "POST /auth/logout":
+      res.end("POST /logout");
+      break;
+  }
+  return;
+}
+
+function crudRouter(req, res) {
+  switch (`${req.method} ${req.url}`) {
+    case "GET /api/data/list":
+      res.end("GET /api/data/list");
+      break;
+    case "POST /api/data":
+      res.end("POST /api/data");
+      break;
+    case "GET /api/data/:id":
+      res.end("GET /api/data/:id");
+      break;
+    case "DELETE /api/data/:id":
+      res.end("DELETE /api/data/:id");
+      break;
+    case "UPDATE /api/data/:id":
+      res.end("UPDATE /api/data/:id");
+      break;
+  }
+  return;
+}
+
+function errorHandler(req, res) {
+  return res.end(
+    JSON.stringify(
+      {
+        code: 404,
+        message: "not found route",
+        url: req.url
+      },
+      null,
+      4
+    )
+  );
 }
